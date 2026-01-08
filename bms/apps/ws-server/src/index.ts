ad24b0e6af4c099838f3df3a21764b4587d55288
Abhidb2007 <abhishekdb2210@gmail.com>
2026-01-08 16:05:13 +0530

    add ws server code

---
+++ bms/apps/ws-server/src/index.ts
@@ -0,0 +1,14 @@
+import {WebSocketServer} from "ws";
+import {client} from "@repo/db/client";
+const server=new WebSocketServer({
+    port:30001
+})
+server.on("connection",(socket)=>{
+    client.user.create({
+        data:{
+            username:Math.random().toString(),
+            password:Math.random().toString()
+        }
+    })
+    socket.send("hi there you are connected to the server");
+})
