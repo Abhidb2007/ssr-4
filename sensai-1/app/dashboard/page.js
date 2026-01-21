8aabb11aaebd32b4d5b94676dda79e1e8340c513
Abhidb2007 <abhishekdb2210@gmail.com>
2026-01-21 23:51:15 +0530

    code

---
+++ sensai-1/app/dashboard/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardPage = () => {
+    return (
+        <div className="container mx-auto mt-24">
+            <h1 className="text-3xl font-bold">Dashboard</h1>
+            <p className="mt-4">Welcome to your dashboard!</p>
+        </div>
+    )
+}
+
+export default DashboardPage
