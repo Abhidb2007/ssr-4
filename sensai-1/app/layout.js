b23f7d68348ad6057eb4596c5b210cba99ef7146
Abhidb2007 <abhishekdb2210@gmail.com>
2026-01-23 21:56:04 +0530

    code

--- sensai-1/app/layout.js
+++ sensai-1/app/layout.js
@@ -1,53 +1,48 @@
-import { Inter } from "next/font/google"
-import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import Header from "@/components/header"
-import { ClerkProvider } from "@clerk/nextjs"
-const inter = Inter({ subsets: ["latin"] })
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sensai-Ai Career Coach",
+  title: "AI Career Coach",
   description: "",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider appearance={{ theme: "dark" }}>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${inter.className}`}>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
-            {/* header */}
             <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
 
-            <main className="min-h-screen">
-              {children}
-            </main>
-
-            {/* footer */}
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with ❤️ by RoadsideCoder</p>
+                <p>Made with 💗 by RoadsideCoder</p>
               </div>
             </footer>
           </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
--- sensai-1/lib/checkUser.js
+++ sensai-1/lib/checkUser.js
@@ -1,4 +1,5 @@
 import { currentUser } from "@clerk/nextjs/server";
+import { db } from "./prisma";
 
 export const checkUser = async () => {
     const user = await currentUser();
@@ -7,5 +8,30 @@ export const checkUser = async () => {
         return null;
     }
 
-    return user;
+    try {
+        const loggedInUser = await db.user.findUnique({
+            where: {
+                clerkUserId: user.id,
+            },
+        });
+
+        if (loggedInUser) {
+            return loggedInUser;
+        }
+
+        const name = `${user.firstName} ${user.lastName}`;
+
+        const newUser = await db.user.create({
+            data: {
+                clerkUserId: user.id,
+                name,
+                imageUrl: user.imageUrl,
+                email: user.emailAddresses[0].emailAddress,
+            },
+        });
+
+        return newUser;
+    } catch (error) {
+        console.log(error.message);
+    }
 };
